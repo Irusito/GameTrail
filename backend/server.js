2b740cd206835f2e77159ccacae0fc0f5276a816
Irusito <ruben.perez.valdes@gmail.com>
2026-06-08 19:26:53 +0200

    Configurar backend con MongoDB local

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,30 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+
+const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+// Conexión a MongoDB
+connectDB();
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Rutas
 app.use("/api/auth", authRoutes);
 
-const PORT = 5000;
+app.get("/", (req, res) => {
+  res.send("Backend funcionando");
+});
+
+// Servidor
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
